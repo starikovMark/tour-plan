7f46c5efc7bcec67d1694a6633b635ab25357cbc
mark-starikov <supermark2005@mail.ru>
2022-07-17 22:40:10 +0700

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
     // Navigation arrows
     navigation: {
       nextEl: '.slider-button--next',
       prevEl: '.slider-button--prev',
+    },
+  });
+  const reviewsSlider = new Swiper('.reviews-swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-swiper__button--next',
+      prevEl: '.reviews-swiper__button--prev',
     },
   });
